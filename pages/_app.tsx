e7769b0598620a44306274f9d15556ce6010dd62
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-01-17 01:31:52 +0530

    vercel analystics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 import MouseContextProvider from '../context/mouse-context'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <MouseContextProvider>
     <Component {...pageProps} />
+    <Analytics />
   </MouseContextProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import MainContainer from "../containers/MainContainer";
 // import Cursor from "../utils/Cursor/Cursor";
 import Cursor from "../utils/Cursor/Cursor";
